55d2e5db87b8319c3dde7e50343dd8acbbb4132e
horlumiday <oluwadareysamuel@yahoo.com>
2019-04-01 09:33:32 +0100

    update api link url

--- js/app.js
+++ js/app.js
@@ -11,3 +11,30 @@ function eventListener() {
 }
 
 
+function loadNames() {
+
+	//get elements from the dom
+	const amount = document.querySelector('input[type="number"]');
+	const country = document.querySelector('#country');
+	const gender = document.querySelector('#gender');
+	const errorMessage = document.querySelector('.error-message');
+
+	//store api link
+	let url = 'https://uinames.com/api/?';
+
+
+	if (country.value !== '' && gender.value !== '') {
+
+		//add region and gender to link
+		url += `region=${country.value}&gender=${gender.value}&`;
+
+		//add amount to link
+		url += `amount=${amount.value}`;
+
+
+	}
+
+}
+
+
+
